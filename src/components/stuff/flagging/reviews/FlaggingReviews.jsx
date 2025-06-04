cf03fabba68ec4fdb988e5bf5e869df489735db5
Wiktoria Wróbel <01180686@pw.edu.pl>
2025-06-04 22:41:38 +0200

    feat(flag): add flagged listings and reviews links to navbar; update user statistics import path

---
+++ src/components/stuff/flagging/reviews/FlaggingReviews.jsx
@@ -0,0 +1,10 @@
+function FlaggingReviews() {
+  return (
+    <div className="flagging-reviews">
+      <h1>Flagging Reviews</h1>
+      <p>This component is under construction.</p>
+    </div>
+  );
+}
+
+export default FlaggingReviews;
